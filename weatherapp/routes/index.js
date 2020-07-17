488680b655106c04bae996cb4d1fb628e7401d1b
breaker2l <34549956+breaker2l@users.noreply.github.com>
2020-07-17 10:34:50 -0700

    up

---
+++ weatherapp/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+const app = require('../app');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Swivt' });
+  //res.sendFile(public/index.html)
+});
+
+module.exports = router;
